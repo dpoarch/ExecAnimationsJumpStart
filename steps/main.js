f6c1849c224f796bf96fc8ea4e75c08611a95b4e
dpoarch <davidpoarch@outlook.com>
2019-09-06 03:57:24 +0800

    finished animate on scroll and intro text updated

--- steps/main.js
+++ steps/main.js
@@ -32,4 +32,52 @@ function animateAll(){
   fsRight4.play();
 }
 
-animateAll();
+
+
+window.addEventListener("load", function() {
+  document.querySelector('body').classList.add('loaded');
+
+  var listClassIndices = function(name,i,n){
+    var str ="";
+    for (x = i; x <= n; x++) {
+      str += name+"-"+i+", ";
+    }
+    return str.substring(0, str.length - 2); 
+  }
+
+  var bindIdtoAnimateClass = function(name){
+    $('#'+name).addClass(name);
+  }
+
+  var bindIdstoAnimateClass = function(name,i,n){
+    $(listClassIndices('#'+name, i, n)).addClass(name);
+  }
+
+  //Event
+  window.addEventListener('message', function(event) {
+      if (typeof event.data == 'object' && event.data.visible == true) {
+        //bindIdstoAnimateClass('animate-steps-scale', 1, 6);
+        //bindIdstoAnimateClass('animate-steps-fade-down', 1, 3);
+        //bindIdstoAnimateClass('animate-steps-fade-left', 1, 4);
+        bindIdstoAnimateClass('animate-steps-fade-right', 1, 3);
+        //bindIdstoAnimateClass('animate-steps-text-focus', 1, 6);
+        $('#animate-steps-scale-1').addClass('animate-steps-scale');
+        $('#animate-steps-scale-2').addClass('animate-steps-scale');
+        $('#animate-steps-scale-3').addClass('animate-steps-scale');
+        $('#animate-steps-scale-4').addClass('animate-steps-scale');
+        $('#animate-steps-scale-5').addClass('animate-steps-scale');
+        $('#animate-steps-text-focus-1').addClass('animate-steps-text-focus');
+        $('#animate-steps-text-focus-2').addClass('animate-steps-text-focus');
+        $('#animate-steps-text-focus-3').addClass('animate-steps-text-focus');
+        $('#animate-steps-text-focus-4').addClass('animate-steps-text-focus');
+        $('#animate-steps-text-focus-5').addClass('animate-steps-text-focus');
+        $('#animate-steps-fade-down-1').addClass('animate-steps-fade-down');
+        $('#animate-steps-fade-down-2').addClass('animate-steps-fade-down');
+        $('#animate-steps-fade-left-1').addClass('animate-steps-fade-left');
+        $('#animate-steps-fade-left-2').addClass('animate-steps-fade-left');
+        $('#animate-steps-fade-left-3').addClass('animate-steps-fade-left');
+
+        animateAll();
+      }
+  }, false);
+});
