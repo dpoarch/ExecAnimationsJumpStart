fe67133bceb248ac612793c7bef286ca3bffc498
dpoarch <davidpoarch@outlook.com>
2019-10-09 15:36:29 -0400

    color class changes

--- steps/main.js
+++ steps/main.js
@@ -59,39 +59,12 @@ window.addEventListener("load", function() {
     $(listClassIndices('#'+name, i, n)).addClass(name);
   }
 
-  // old aos
-  // window.addEventListener('message', function(event) {
-  //     if (typeof event.data == 'object' && event.data.visible == true) {
-  //       bindIdstoAnimateClass('animate-steps-fade-right', 1, 3);
-  //       $('#animate-steps-scale-1').addClass('animate-steps-scale');
-  //       $('#animate-steps-scale-2').addClass('animate-steps-scale');
-  //       $('#animate-steps-scale-3').addClass('animate-steps-scale');
-  //       $('#animate-steps-scale-4').addClass('animate-steps-scale');
-  //       $('#animate-steps-scale-5').addClass('animate-steps-scale');
-  //       $('#animate-steps-text-focus-1').addClass('animate-steps-text-focus');
-  //       $('#animate-steps-text-focus-2').addClass('animate-steps-text-focus');
-  //       $('#animate-steps-text-focus-3').addClass('animate-steps-text-focus');
-  //       $('#animate-steps-text-focus-4').addClass('animate-steps-text-focus');
-  //       $('#animate-steps-text-focus-5').addClass('animate-steps-text-focus');
-  //       $('#animate-steps-fade-down-1').addClass('animate-steps-fade-down');
-  //       $('#animate-steps-fade-down-2').addClass('animate-steps-fade-down');
-  //       $('#animate-steps-fade-left-1').addClass('animate-steps-fade-left');
-  //       $('#animate-steps-fade-left-2').addClass('animate-steps-fade-left');
-  //       $('#animate-steps-fade-left-3').addClass('animate-steps-fade-left');
-  //       animateAll();
-  //     }
-  // }, false);
-
-    // new suggested aos
     window.iFrameResizer = {
       targetOrigin: '*',
       onMessage: function(message) 
       {
-          var messageData = JSON.parse(message);                           
-          //$("#display").text("scrollRatio: " + messageData["scrollRatio"] + " verticalDisplayRatio:" + messageData["verticalDisplayRatio"])
-          //console.log(message);
+          var messageData = JSON.parse(message);    
           if(messageData["verticalDisplayRatio"]>0){
-            //$('.animate-steps-hide').removeClass('animate-steps-hide');
             $('#animate-steps-fade-right-1').addClass('animate-steps-fade-right');
             $('#animate-steps-fade-right-2').addClass('animate-steps-fade-right');
             $('#animate-steps-fade-right-3').addClass('animate-steps-fade-right');
@@ -112,27 +85,6 @@ window.addEventListener("load", function() {
             $('#animate-steps-fade-left-3').addClass('animate-steps-fade-left');
             animateAll(); 
           }
-          // else if (messageData["verticalDisplayRatio"]<0.5||messageData["verticalDisplayRatio"]>1){
-          //   $('#animate-steps-fade-right-1').removeClass('animate-steps-fade-right');
-          //   $('#animate-steps-fade-right-2').removeClass('animate-steps-fade-right');
-          //   $('#animate-steps-fade-right-3').removeClass('animate-steps-fade-right');
-          //   $('#animate-steps-scale-1').removeClass('animate-steps-scale');
-          //   $('#animate-steps-scale-2').removeClass('animate-steps-scale');
-          //   $('#animate-steps-scale-3').removeClass('animate-steps-scale');
-          //   $('#animate-steps-scale-4').removeClass('animate-steps-scale');
-          //   $('#animate-steps-scale-5').removeClass('animate-steps-scale');
-          //   $('#animate-steps-text-focus-1').removeClass('animate-steps-text-focus');
-          //   $('#animate-steps-text-focus-2').removeClass('animate-steps-text-focus');
-          //   $('#animate-steps-text-focus-3').removeClass('animate-steps-text-focus');
-          //   $('#animate-steps-text-focus-4').removeClass('animate-steps-text-focus');
-          //   $('#animate-steps-text-focus-5').removeClass('animate-steps-text-focus');
-          //   $('#animate-steps-fade-down-1').removeClass('animate-steps-fade-down');
-          //   $('#animate-steps-fade-down-2').removeClass('animate-steps-fade-down');
-          //   $('#animate-steps-fade-left-1').removeClass('animate-steps-fade-left');
-          //   $('#animate-steps-fade-left-2').removeClass('animate-steps-fade-left');
-          //   $('#animate-steps-fade-left-3').removeClass('animate-steps-fade-left');
-          //   resetAll(); 
-          // }
       },        
   } 
 });
